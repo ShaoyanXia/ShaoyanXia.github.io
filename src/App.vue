6ad6a99289778331cc9cce8a79a7216f215e560e
夏绍炎 <xiashaoyan@nj-geo.com>
2019-12-27 10:17:45 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <mainpage msg="欢迎来到夏绍炎的个人Blog"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import mainpage from './components/main_page.vue'
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+	mainpage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/main_page.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<p class="title">{{msg}}</p>
+	</div>
+</template>
+
+<script>
+	export default {
+	  name: 'mainpage',
+	  props: {
+	    msg: String
+	  }
+	}
+</script>
+
+<style>
+	.title{
+		font-size: larger;
+	}
+</style>
